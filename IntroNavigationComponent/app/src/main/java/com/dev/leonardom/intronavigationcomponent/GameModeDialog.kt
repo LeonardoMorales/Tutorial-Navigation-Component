e773a6017a0e03a947fbb83c29688714bd846bde
LeonardoMSycnos <leonardom@sycnos.com>
2021-01-09 19:15:23 -0600

    Inclusión de librerías y fragmentos necesarios

---
+++ IntroNavigationComponent/app/src/main/java/com/dev/leonardom/intronavigationcomponent/GameModeDialog.kt
@@ -0,0 +1,34 @@
+package com.example.viewbinding
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+
+class GameModeDialog: DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val gameModeOptions = arrayOf("Modo 1", "Modo 2", "Modo 3", "Modo 4")
+        var optionSelected = gameModeOptions[0]
+
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+
+            builder
+                .setTitle("Dialogo de Prueba")
+                .setSingleChoiceItems(gameModeOptions, 0){ dialog, posicion ->
+                    optionSelected = gameModeOptions[posicion]
+                }
+                .setPositiveButton("ACEPTAR") { dialog, id ->
+
+                }
+                .setNegativeButton("CANCELAR"){ dialog, id ->
+                    dialog.dismiss()
+                }
+
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+
+    }
+}
+
